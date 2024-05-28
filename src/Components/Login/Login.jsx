56a2f6b30cb2ec3a9ff6124b10cc3bc67e109f1c
Anmy Josy <anmyjosy@gmail.com>
2024-05-28 16:27:44 +0530

    third

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://https-github-com-anmyjosy-cpi.onrender.com/login', { email, password }, { withCredentials: true })
+    axios.post('http://localhost:3001/login', { email, password }, { withCredentials: true })
       .then(result => {
         console.log(result);
         if (result.data === "no record existed") {
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -14,7 +14,7 @@ function Register(){
 
   const handleSubmit=(e)=>{
     e.preventDefault()
-    axios.post('https://https-github-com-anmyjosy-cpi.onrender.com/register',{name,email,password,role,purpose})
+    axios.post('http://localhost:3001/register',{name,email,password,role,purpose})
     .then(response => {
       if(response.data.message==="user already existed"){
         alert('user already existed');
