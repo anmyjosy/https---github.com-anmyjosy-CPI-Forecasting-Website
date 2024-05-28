b83dfba912c37a77720555e050514fd763891b84
Anmy Josy <anmyjosy@gmail.com>
2024-05-28 13:51:38 +0530

    third

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: [""],
+    origin: ["castfore.vercel.app"],
     methods:['POST',"GET"],
     credentials: true
 }));
