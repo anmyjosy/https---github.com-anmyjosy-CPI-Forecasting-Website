e73e607864c4ff2e72941c6455eed356314a6614
Anmy Josy <anmyjosy@gmail.com>
2024-05-29 20:40:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post("/login", (req, res) => {
     .then(user => {
       if (user) {
         if (user.password === password) {
-          const token = jwt.sign({ email: user.email }, process.env.KEY, { expiresIn: '1s' });
+          const token = jwt.sign({ email: user.email }, process.env.KEY, { expiresIn: '70s' });
           res.cookie('token', token, { httpOnly: true ,secure: true,sameSite: 'None'});
           res.json("success");
         } else {
